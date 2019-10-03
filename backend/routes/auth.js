d925606dc2e18e7b432b1fee7823738c48dfd6f9
Nikolai Nossulenko <nn@Nikolais-MBP.localdomain>
2019-10-03 15:33:21 +0200

    setup user auth validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { signup } = require('../controllers/auth');
 
-router.post('/signup', signup);
+// Validators
+
+const {runValidation} = require('../validators');
+const {userSignupValidator} = require('../validators/auth');
+
+
+//if the validation is passed only then will the signup function be invoked
+
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
---
+++ backend/validators/auth.js
@@ -0,0 +1,15 @@
+const {check} = require('express-validator');
+exports.userSignupValidator = [
+
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Naam is verplicht!'),
+    check('email')
+        .not()
+        .isEmpty()
+        .withMessage('Vul een geldig email adres in!'),
+    check('password')
+        .isLength({min: 6})
+        .withMessage('Uw password moet minstens 6 karakters lang zijn')
+];
