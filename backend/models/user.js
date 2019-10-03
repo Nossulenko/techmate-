74efa79c9b2618de108cdf038b62af990942847e
Nikolai Nossulenko <nn@Nikolais-MBP.localdomain>
2019-10-03 16:05:33 +0200

    added password encryption

--- backend/models/user.js
+++ backend/models/user.js
@@ -76,4 +76,43 @@ const userSchema = new mongoose.Schema(
 
     );
 
+
+userSchema.virtual('password')
+    .set(function(password){
+        //create a temp var called password
+        this._password = password
+        // generate salt
+        this.salt = this.makeSalt()
+        // encryptPassword
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this.password;
+    });
+
+userSchema.methods = {
+
+    // compare passwords
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    // hash the passwords
+    encryptPassword: function(password) {
+
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch(err) {
+            return '';
+        }
+    },
+        // generate salt
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
+};
+
 module.exports = mongoose.model('User', userSchema);
