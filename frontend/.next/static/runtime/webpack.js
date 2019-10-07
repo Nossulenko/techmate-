1e21d5d5bc1cb5ffdccd6daf7f152af67c5de4e3
Nikolai Nossulenko <nn@Nikolais-MBP.localdomain>
2019-10-07 16:48:08 +0200

    added signup form(working)

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "25a410ee923d124d0909";
+/******/ 	var hotCurrentHash = "855c001b522f0e3fdd22";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Auth/SignupComponent.js
+++ frontend/components/Auth/SignupComponent.js
@@ -1,31 +1,71 @@
+import { useState } from  'react';
+
 const SignupComponent = () => {
-    const handleSubmit = (e) => {
+    const [values, setValues] = useState({
+
+        name: 'Nikolai',
+        email: 'nikolai@test.be',
+        password: '123456',
+        error: '',
+        loading: false,
+        message: '',
+        showForm: true
+
+    });
+
+    const {name, email, password, error, loading, message, showForm} = values;
+
+    const handleSubmit = e => {
         e.preventDefault();
-        console.log('handle submit')
+        console.table({name, email, password, error, loading, message, showForm});
     };
-
-    const handleChange = (e) => {
-
-        e.preventDefault();
-        console.log(e.target.value)
+    const handleChange = name => e => {
+        setValues({...values, error: false, [name]: e.target.value});
     };
 
     const signupForm = () => {
 
         return (
             <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <input onChange={handleChange} type="text" className="form-control" placeholder="Type your name"/>
-            </div>
+
+                <div className="form-group">
+                    <input value={name}
+                           onChange={handleChange('name')}
+                           type="text"
+                           className="form-control"
+                           placeholder="Type your name"/>
+                </div>
+
+                <div className="form-group">
+                    <input
+                        value= {email}
+                        onChange= {handleChange('email')}
+                        type="email"
+                        className="form-control"
+                        placeholder="Type your email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        value={password}
+                        onChange={handleChange('password')}
+                        type="password" className="form-control"
+                        placeholder="Type your password"
+                    />
+                </div>
+                <div>
+                    <button className="btn btn-primary">Registreren</button>
+                </div>
+
+
             </form>
-        )
+        );
     };
 
-    return (
-        <React.Fragment>
-            {signupForm()}
-        </React.Fragment>
-    )
+
+    return <React.Fragment>{signupForm()}</React.Fragment>;
+
 };
 
 export default SignupComponent;
